d1a875785d5bda19bcff315eca4936f5cf529c7c
rafaelgdgs <rafagsique@hotmail.com>
2024-05-15 03:40:04 -0300

    feat(*): base video project

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+#[derive(Debug, PartialEq)]
+enum State {
+    Mario,
+    SuperMario,
+    FireMario,
+    CapeMario,
+}
+
+#[derive(Debug)]
+enum Transition {
+    Mushroom,
+    Flower,
+    Feather,
+}
+
+#[derive(Debug)]
+struct Player {
+    state: State,
+}
+
+impl Player {
+    fn new() -> Self {
+        Self {
+            state: State::Mario,
+        }
+    }
+    fn collect(&mut self, power: Transition) {
+        match (&self.state, power) {
+            (State::Mario, Transition::Mushroom) => self.state = State::SuperMario,
+            (_, Transition::Flower) => self.state = State::FireMario,
+            (_, Transition::Feather) => self.state = State::CapeMario,
+            (_, Transition::Mushroom) => {}
+        }
+    }
+}
+
+fn main() {
+    let mut dplayer = Player::new();
+    dplayer.collect(Transition::Mushroom);
+    dplayer.collect(Transition::Flower);
+    dplayer.collect(Transition::Feather);
+    dplayer.collect(Transition::Flower);
+    assert!(dplayer.state == State::CapeMario);
+}
